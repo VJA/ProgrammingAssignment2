8cb68708447ed960655233acd2e262b1731f0c61
VJA <vijay_anand_er@yahoo.com>
2015-03-23 01:30:35 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,24 +2,26 @@
 ## The function will check if the inverse is already created
 ## If the inverse is already created, it will fetch the inversed matrix from cache memory
 ## If it is not calculated already, the function will inverse the matrix and assign it to cache for later purpose
-## next time, it will retrieve the cached memory
+## next time, it will retrieve from the cached memory
 ## This function is useful when the inverse has to be used multiple times 
-## As it will be stored in the cache, can be retrived everytime and need not derive everytime
+## As it will be stored in the cache, can be retrived everytime and need not derive everytime which saves times and memory
 
+## .....................................makeCacheMatrix Function .......................
 ## define Caching function which will return a list of four functions 
 ## the functions in the list will be used to call defining or returning the martrix or it's inversed value
-
-
 makeCacheMatrix <- function(x = matrix()) {
 
   i <- NULL   # Assigning the Inverse Matrix variable
-  
+          ## ......... set function ......
   set <- function(y) {  # Defining the function to create the matrix
     x <<- y             # Assigning the matrix value
     i <<- NULL
   }
+          ## ..... get function .....
   get <- function() x   # Function to return the matrix
+          ## .......setinv function.....
   setinv <- function(solve) i <<- solve   # Function to assing the inversed matrix variable(Caching for later purpose)
+          ##........getinv function.......
   getinv <- function() i  # Returns the inversed matrix
   
   # Returns a list of set, get, setinv, getinv functions
@@ -30,13 +32,13 @@ makeCacheMatrix <- function(x = matrix()) {
   
 }
 
-
+##.....................cacheSolve......................
 ## function to check if the inverse is already determined and determine if needed
 
 cacheSolve <- function(x, ...) {    # Defining the function
-  i <- x$getinv()   # Assigning the inversed value using getinv function of the list x
+  i <- x$getinv()   # Assigning the inversed value using getinv function of the list "x"
   if(!is.null(i)) { # Checks if the matrix is already inversed
-    message("getting cached data")  # Prints if the inverse is already calculated
+    message("getting cached data")  # Prints if the inverse is already calculated just as a confirmation it is using cache
     return(i)   # Retuns the inversed matrix and exits the function
   }
   
